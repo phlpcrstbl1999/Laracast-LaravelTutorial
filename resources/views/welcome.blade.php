b252fc376c298542b61836872bb34a6e1c7ef2c6
Philip Cristobal <philipcristobal06@gmail.com>
2023-10-26 10:07:39 +0800

    Make a Route and Link to it

--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html>
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-    <title>My Blog</title>
-    <body>
-        <h1>Hello World</h1>
-    </body>
-</html>
